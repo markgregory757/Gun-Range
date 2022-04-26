01594e272950c6b946b65ab20e3801038ad68878
Mark Gomez <mark.g.gomez@gmail.com>
2022-04-25 22:17:55 -0700

    Created files and installed packages

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello")
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://Bloggers:Bloggers@cluster0.hfjai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
