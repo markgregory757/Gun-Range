e8624fef047be8fea9a2575b5046389fd20fd55a
Mark Gomez <mark.g.gomez@gmail.com>
2022-04-26 00:50:25 -0700

    drafted model

--- models/Person.js
+++ models/Person.js
@@ -1,3 +1,36 @@
 const mongoose = require('mongoose');
 
- const Person = mongoose.model("Person", )
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    image_url: String,
+    
+    abilities: [{}]
+
+})
+
+ module.exports = mongoose.model("Person", personSchema);
+
+ 
+/* DEMO */
+//  const personSchema = new mongoose.Schema({
+//     name: String
+// })
+
+// personSchema.methods.shoot = function shoot() {
+//     const fire = this.name
+//     ? "At noon " + this.name + " will dule."
+//     : "No dule"
+
+//     console.log(fire)
+// }
+
+// const Person = mongoose.model("Person", personSchema)
+
+// const shooter = new Person({name : "Wyatt"})
+
+// shooter.shoot()
+
+// console.log("My names: ", shooter)
+
+// shooter.save().then(() => console.log("Bang!"));
