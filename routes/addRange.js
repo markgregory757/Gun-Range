99e3b51f974061ab42774f17a9c8e7d2b6fd2cce
Mark Gomez <mark.g.gomez@gmail.com>
2022-04-27 20:10:33 -0700

    updated routes and hbs pages

--- routes/addRange.js
+++ routes/addRange.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.redirect('addRange');
+    res.render('addRange');
 });
 
 module.exports = router
