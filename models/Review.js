01a962f944def45253c4534a2953d1bad3fadeee
David Scott <David.Scott808@gmail.com>
2022-04-27 16:51:15 -1000

    updated models

---
+++ models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const range = require('./Range')
+const person = require('./Person')
+const comment = require('./Comment')
+
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true],
+    minLength: 50
+  },
+  reviewer: [{ 
+    type: Schema.Types.ObjectId, 
+    ref:'Person'
+  }],
+  range: [{ 
+    type: Schema.Types.ObjectId, 
+    ref:'Range'
+  }],
+  comments: [{ 
+    type: Schema.Types.ObjectId, 
+    ref:'Comment'
+  }],
+})
+
+
+module.exports = mongoose.model("Review", reviewSchema);
