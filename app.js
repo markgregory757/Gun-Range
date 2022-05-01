b312be4c9a363fe8ae6237cc612df2fb5865aa88
David Scott <David.Scott808@gmail.com>
2022-04-30 18:13:46 -1000

    error 500 on add review

--- app.js
+++ app.js
@@ -44,14 +44,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(
-//   session({
-//     secret: "secret",
-//     httpOnly: true,
-//     secure: true,
-//     resave: true,
-//     saveUnitialized: true
-//   }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
