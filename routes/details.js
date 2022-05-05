e37259331a49c93d44655a9ead40227392fa0b63
Mark Gomez <mark.g.gomez@gmail.com>
2022-05-04 18:06:35 -0700

    updated details.js

--- routes/details.js
+++ routes/details.js
@@ -7,9 +7,11 @@ const async = require("hbs/lib/async");
 const id = require("../middleware/getRangeId" )
 
 router.get("/:id",  async (req, res) => {
-    
+    console.log('reached for id', req.params)
     const id = req.params.id;
-    const aRange = await Range.findOne({id: id})
+    // console.log(id)
+    const aRange = await Range.findById(id)
+    console.log(aRange)
     res.render('details', {title: "Details", range: aRange});
 });
 
