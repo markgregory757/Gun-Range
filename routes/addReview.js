ce52ef2bd9bd0402c28ec9c946f5240d26080a5f
David Scott <David.Scott808@gmail.com>
2022-05-07 11:04:04 -1000

    working addReview render

--- routes/addReview.js
+++ routes/addReview.js
@@ -5,7 +5,8 @@ const addRange = require("../views/addRange");
 const jwt = require("jsonwebtoken");
 const async = require("hbs/lib/async");
 const id = require("../middleware/getRangeId" )
-
+const loginUser = require("../middleware/loginUser");
+const Person = require('../models/Person');
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
@@ -16,7 +17,17 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  const id = req.params.id
+  let {review, reviewer, range} = req.body;
+  console.log('req: ',req.body)
 
+  // const newReview = new Range({
+  //   review,
+  //   reviewer,    
+  //   range
+  // })
+  // await newReview.save();
+  // res.render("addReview");
   console.log(review, reviewer, range)
 
   const newRange = new Range({
