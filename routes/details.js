7470ecb46d17b75366bcada1814f6db4ac5a0204
David Scott <David.Scott808@gmail.com>
2022-05-09 12:10:46 -1000

    reviws up and running

--- routes/details.js
+++ routes/details.js
@@ -4,15 +4,19 @@ const Range = require("../models/Range");
 const addRange = require("../views/addRange");
 const jwt = require("jsonwebtoken");
 const async = require("hbs/lib/async");
-const id = require("../middleware/getRangeId" )
+const id = require("../middleware/getRangeId" );
+const Review = require("../models/Review");
 
 router.get("/:id", async (req, res) => {
-    console.log('reached for id', req.params)
+    // console.log('reached for id', req.params)
     const id = req.params.id;
     // console.log(id)
-    const aRange = await Range.findById(id)
-    console.log(aRange)
-    res.render('details', {title: "Details", range: aRange});
+    const aRange = await Range.findById(id).populate('review')
+      
+    const theseReviews = await Review.find({range: aRange.name})
+    console.log(theseReviews)
+
+    res.render('details', {title: "Details", range: aRange, reviews: theseReviews});
 });
 
 router.get("/personDetails/:id", async (req, res) => {
